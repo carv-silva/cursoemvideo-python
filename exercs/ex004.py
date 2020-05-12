8f699e0378e015ed4d3c1c9c334779218b22ca15
Carlos Vinicius <car_los.vinicius@hotmail.com>
2020-05-12 15:28:26 -0300

    utilizando função fstring interpolada

--- exercs/ex004.py
+++ exercs/ex004.py
@@ -3,13 +3,13 @@ primário e todos as informacoes possiveis sobre ela'''
 
 n = input('Digite algo: ')
 
-print('O tipo desse valor é:{} '.format(type(n)))
-print('Só tem espaços ? {}'.format(n.isspace()))
-print('É um numero? {}'.format(n.isnumeric()))
-print('É um alfabetico? {}' .format(n.isalpha()))
-print('É um alfanumerico? {}'.format(n.isalnum()))
-print('Esta em letra maiscula? {}' .format(n.isupper()))
-print('Esta em letra maiscula? {}'.format(n.islower()))
-print('Esta capitalizada? {}' .format(n.istitle()))
+print(f'O tipo desse valor é: {type(n)}')
+print(f'Só tem espaços? {n.isspace()}')
+print(f'É um número? {n.isnumeric()}')
+print(f'É um alfabético? {n.isalpha()}')
+print(f'É um alfanumérico? {n.isalnum()}')
+print(f'Esta em letra maiúscula? {n.isupper()}')
+print(f'Esta em letra minúscula? {n.islower()}')
+print(f'Esta capitalizada? {n.istitle()}')
 
 cor = {'pretao_loko': '\033[;1;30;44m'}
